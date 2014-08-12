29d400ec7019029a9cb7b42c4281c2972aa4090e
Manuel Huber <manuel.h87@gmail.com>
2014-08-12 17:41:33 +0100

    Add restart link and command line parsing

--- shoutsvr.py
+++ shoutsvr.py
@@ -15,7 +15,7 @@ import select
 
 __author__ = 'Manuel Huber'
 __copyright__ = "Copyright (c) 2014 Manuel Huber."
-__version__ = '0.1'
+__version__ = '0.2'
 __docformat__ = "restructuredtext en"
 
 _DEFAULT_LOG_FORMAT = "%(name)s : %(threadName)s : %(levelname)s \
@@ -35,6 +35,9 @@ class WebRequestHandler (BaseHTTPRequestHandler):
             self._get_main()
         elif (self._path == "/stdout") and self._param.has_key("line"):
             self._get_line(int(self._param['line'][0]))
+        elif self._path == "/start":
+            self.server.proc.start()
+            self._get_main()
         else:
             self.send_error(404)
 
@@ -60,7 +63,8 @@ class WebRequestHandler (BaseHTTPRequestHandler):
 <body>
 <pre id="console">
 </pre>
-<span id="end">END</span>
+<hr>
+<a id="console_restart" href="/start">Restart task</a>
 <script type="text/javascript">
 
 var co = {
@@ -89,7 +93,6 @@ var co = {
 		var c = $('#console');
 		var pos = c.height() + c.position().top
 		$('html, body').animate({
-//			scrollTop: parseInt($('#end').offset().top)
 			scrollTop: pos
 		}, 200);
 	}
@@ -109,24 +112,45 @@ class Process (object):
     S_FIN_STDERR = 2
     S_FINISHED = 3
     S_PENDING = 0
+    S_TERM_BIT = 4
+    S_STOPPED = 7
 
     def __init__ (self, args):
-        self._p = subprocess.Popen(args, stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-        self._thread_out = threading.Thread(target=self._capture,
-                                            args=(self._p.stdout, self.S_FIN_STDOUT, 'O'))
-        self._thread_err = threading.Thread(target=self._capture,
-                                            args=(self._p.stderr, self.S_FIN_STDERR, 'E'))
+        self._p = None
+        self._args = args
         self._lb_lock = threading.RLock()
+        self._start_lock = threading.RLock()
         self._buffer = list()
         self._max_line = 0
-        self._state = self.S_PENDING
-        self._thread_out.start()
-        self._thread_err.start()
+        self._state = self.S_STOPPED
+
+    def _start_locked (self):
+        with self._lb_lock:
+            if self._state != self.S_STOPPED:
+                logging.debug("Can't restart; process still running")
+                return False
+            self._p = subprocess.Popen(self._args, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+            self._thread_out = threading.Thread(target=self._capture,
+                                                args=(self._p.stdout, self.S_FIN_STDOUT, 'O'))
+            self._thread_err = threading.Thread(target=self._capture,
+                                                args=(self._p.stderr, self.S_FIN_STDERR, 'E'))
+            self._buffer = list()
+            self._max_line = 0
+            self._state = self.S_PENDING
+            self._thread_out.start()
+            self._thread_err.start()
+            return True
+
+    def start (self):
+        with self._start_lock:
+            self._cleanup_locked()
+            return self._start_locked()
 
     def _capture (self, fobj, fin_flag, id_):
         poller = select.poll()
         poller.register(fobj, select.POLLIN | select.POLLERR | select.POLLHUP)
+        process = self._p
         while True:
             for fd, flags in poller.poll(-1):
                 if (flags & select.POLLIN):
@@ -138,6 +162,10 @@ class Process (object):
                     with self._lb_lock:
                         self._state |= fin_flag
                     return
+        process.wait()
+        with self._lb_lock:
+            # TRICKY: Will be set twice but doesn't matter as we join()
+            self._state |= self.S_TERM_BIT
         # Exit function
 
     def get (self, start_line=0):
@@ -146,27 +174,41 @@ class Process (object):
             if start_line < self._max_line:
                 obj['line'] = self._max_line
                 obj['text'] = self._buffer[start_line:self._max_line]
-            elif self._state == self.S_FINISHED:
-                obj['fin'] = True
             else:
                 obj['line'] = self._max_line
+            if self._state == self.S_FINISHED:
+                obj['fin'] = True
+            if self._p is not None:
+                ret = self._p.poll()
+                if ret is not None:
+                    obj['rc'] = ret
         return obj
 
+    def _cleanup_locked (self):
+        if self._p is not None:
+            self._thread_out.join()
+            self._thread_err.join()
+            self._p.wait()
+            self._p = None
+
     def cleanup (self):
-        self._thread_out.join()
-        self._thread_err.join()
+        with self._start_lock:
+            self._cleanup_locked()
 
 
 class ConsoleService (object):
-    def __init__ (self, args, port=PORT):
+    def __init__ (self, args, options):
         self._log = logging.getLogger('bash-service')
         self._args = args
-        self._port = port
+        self._port = options.port
+        self._autostart = options.autostart
 
     def start (self):
         self._proc = Process(self._args)
         self._srv = HTTPServer(('', self._port), WebRequestHandler)
         self._srv.proc = self._proc
+        if self._autostart:
+            self._srv.proc.start()
 
         self._log.info("Start webserver")
         try:
@@ -180,17 +222,29 @@ class ConsoleService (object):
 
 def main (argv):
     parser = optparse.OptionParser(
-        usage="usage: %prog [options]",
+        usage="usage: %prog [options] -- command [arguments]",
         version=("%prog " + __version__)
     )
     parser.add_option("--verbose", action="store_const", const=logging.DEBUG,
         dest="verb_level", help="Verbose output (DEBUG)"
     )
+    parser.add_option("-p", "--port",
+        type=int,
+        dest="port",
+        default=PORT,
+        help="Port to listen on"
+    )
+    parser.add_option("--autostart",
+        action="store_true",
+        dest="autostart",
+        default=False,
+        help="Run command once immediately"
+    )
     parser.add_option("--quiet",
-                      action="store_const",
-                      const=logging.ERROR,
-                      dest="verb_level",
-                      help="Non verbose output: only output errors"
+        action="store_const",
+        const=logging.ERROR,
+        dest="verb_level",
+        help="Non verbose output: only output errors"
     )
     parser.set_defaults(version=False, verb_level=logging.INFO)
 
@@ -203,12 +257,11 @@ def main (argv):
         datetime.now().isoformat())
     )
 
-    if len(args) != 0:
-        parser.error("Wrong arguments")
-    else:
-        csrv = ConsoleService(["find", "/", "-name", "*.c"])
-#        csrv = ConsoleService(["ls", "-al"])
-        csrv.start()
+    if len(args) == 0:
+        parser.error("Missing command!")
+
+    csrv = ConsoleService(args, options)
+    csrv.start()
 
 
 if __name__ == '__main__':
